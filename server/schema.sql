3d396d6dbcec36ce2aa3eec9034ae85b32287878
Hack Reactor Students <accounts@hackreactor.com>
2014-11-21 16:40:05 -0800

    set up database

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,28 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- CREATE TABLE messages (
+--    Describe your table here.
+-- );
+
+CREATE TABLE `user` (
+  `id` INT(40) NOT NULL AUTO_INCREMENT,
+  `username` CHAR(40),
+  PRIMARY KEY  (`id`)
+);
+
+CREATE TABLE `message` (
+  `id` INT(40) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY  (`id`),
+  `username` CHAR(40),
+  `text` CHAR(180),
+  `roomname` CHAR(40)
+);
+
+CREATE TABLE `room` (
+  `roomName` CHAR(40),
+  `id` INT(40) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY  (`id`)
 );
 
 /* Create other tables and define schemas for them here! */
